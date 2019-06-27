4402175a6fb0b6f70560ee5041a45b70ef196a15
leszek <se@lubcon.org>
2019-06-27 23:00:30 +0200

    Add config and mail template

---
+++ mainSpreadsheetVolunteer/config.js
@@ -0,0 +1,21 @@
+function getConfig() {
+    return {
+        responsesSpreadsheetId: "",
+        statusSheet: {
+            name: "",
+            startRow: 2,
+            startCol: 1,
+            nameColArr: 0,
+            sheetColArr: 1,
+            messageColArr: 3,
+            subjectColArr: 2,
+        },
+        responsesSheet: {
+            name: "",
+            startRow: 2,
+            startCol: 1,
+            nameColArr: 2,
+            mailColArr: 5,
+        },
+    }
+}
